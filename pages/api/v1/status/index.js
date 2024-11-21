9a3353bd8e5c7e92f1f8e38b0eb51250bf0eae43
thecaKo <caKoonrails@gmail.com>
2024-11-21 14:51:43 -0300

    add database.js and ".env" files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function Status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function Status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({
     nome: "Carlos",
   });
