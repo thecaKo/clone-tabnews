7612d78ee2713d4a9b7b7db50d8acb3d818a3138
thecaKo <cakoonsrails@gmail.com>
2024-11-23 11:57:28 -0300

    changing SQL querys to be more secure

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,26 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function Status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+  const resultDatabaseVersion = await database.query("SHOW server_version;");
+  const resultDatabaseMaxConnection = await database.query("SHOW max_connections;");
+  const resultDatabaseOpenedConnections = await database.query({
+    text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1",
+    values: [databaseName],
+  });
+
+
   response.status(200).json({
-    nome: "Carlos",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: resultDatabaseVersion.rows[0].server_version,
+        max_connections: parseInt(resultDatabaseMaxConnection.rows[0].max_connections),
+        opened_connections: resultDatabaseOpenedConnections.rows[0].count
+      }
+    }
   });
 }
 
