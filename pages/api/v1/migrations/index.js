789cbcf3d35d42a6cd6ae72839b68014d846a5b4
thecaKo <cakoonsrails@gmail.com>
2024-11-25 15:31:47 -0300

    fix /migrations endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,10 +2,15 @@ import migrationsRunner from 'node-pg-migrate'
 import { join } from 'node:path'
 import database from 'infra/database.js'
 
-
 export default async function Migrations(request, response) {
+  const allowedMethods = ["GET", "POST"];
+  if(!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: "Method not allowed"
+    });
+  }
   const dbClient = await database.getNewClient();
-
+  try {
   const defaultMigrationsSettings = {
     dbClient: dbClient,
     dryRun: true,
@@ -17,7 +22,6 @@ export default async function Migrations(request, response) {
 
   if (request.method === 'GET') {
     const migrations = await migrationsRunner(defaultMigrationsSettings);
-    await dbClient.end();
     return response.status(200).json(migrations);
   }
 
@@ -26,9 +30,13 @@ export default async function Migrations(request, response) {
       ...defaultMigrationsSettings,
       dryRun: false,
     });
-    await dbClient.end();
     return response.status(200).json(migrations);
   }
-  return response.status(405).end();
+  } catch(error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
 
